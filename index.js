86b79a7f91cb0a82f4dcddc7e9205679b225feca
RCGR <138417636+rc-gr@users.noreply.github.com>
2023-08-01 22:18:50 +0800

    Add createSquareBoard with createDiv helper

---
+++ index.js
@@ -0,0 +1,24 @@
+function createDiv(classes) {
+  const div = document.createElement('div');
+  
+  if (typeof classes === 'string') {
+    div.classList.add(classes);
+  } else if (typeof classes === 'object') {
+    for (item of classes) {
+      div.classList.add(item);
+    }
+  }
+
+  return div;
+}
+
+function createSquareBoard(containerClassName, size = 16) {
+  const container = document.querySelector(`.${containerClassName}`);
+  for (let rowIndex = 0; rowIndex < size; rowIndex++) {
+    const row = createDiv('row');
+    container.appendChild(row);
+    for (let colIndex = 0; colIndex < size; colIndex++) {
+      row.appendChild(createDiv('cell'));
+    }
+  }
+}
